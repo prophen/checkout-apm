4408d7864859e9d1edd08ddea6a66348dd506642
Nikema Prophet <3941856+prophen@users.noreply.github.com>
2023-03-04 10:31:21 -0800

    Add `fundingSources` array, replace "alternate" with "alternative"

--- public/new-sample-code.js
+++ public/new-sample-code.js
@@ -1,85 +1,80 @@
-// Render the PayPal marks
-paypal.Marks({ fundingSource: "paypal" }).render("#paypal-mark-container");
-paypal.Marks({ fundingSource: "card" }).render("#card-mark-container");
-paypal.Marks({ fundingSource: "paylater" }).render("#paylater-mark-container");
-
-paypal
-  .Buttons({
-    fundingSource: "paypal",
-    // Sets up the transaction when a payment button is clicked
-    createOrder: function () {
-      return fetch("/my-server/create-paypal-order", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        // use the "body" param to optionally pass additional order information
-        // like product skus and quantities
-        body: JSON.stringify({
-          cart: [
-            {
-              sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
-              quantity: "YOUR_PRODUCT_QUANTITY",
-            },
-          ],
-        }),
-      })
-        .then((response) => response.json())
-        .then((order) => order.id);
-    },
-    // Finalize the transaction after payer approval
-    onApprove: function (data) {
-      return fetch("/my-server/capture-paypal-order", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          orderID: data.orderID,
-        }),
-      })
-        .then((response) => response.json())
-        .then((orderData) => {
-          // Successful capture! For dev/demo purposes:
-          console.log(
-            "Capture result",
-            orderData,
-            JSON.stringify(orderData, null, 2)
-          );
-          const transaction = orderData.purchase_units[0].payments.captures[0];
-          alert(
-            "Transaction " +
-              transaction.status +
-              ": " +
-              transaction.id +
-              "\n\nSee console for all available details"
-          );
-          // When ready to go live, remove the alert and show a success message within this page. For example:
-          // var element = document.getElementById('paypal-button-container');
-          // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-          // Or go to another URL:  actions.redirect('thank_you.html');
-        });
-    },
-  })
-  .render("#paypal-button-container");
-paypal
-  .Buttons({
-    fundingSource: "card",
-  })
-  .render("#card-button-container");
-paypal
-  .Buttons({
-    fundingSource: "paylater",
-  })
-  .render("#paylater-button-container");
+const fundingSources = ["paypal", "card", "paylater"];
+fundingSources.forEach((fundingSource) => {
+  // Render the PayPal Marks
+  paypal
+    .Marks({ fundingSource: fundingSource })
+    .render(`#${fundingSource}-mark-container`);
+  // Render PayPal Buttons
+  paypal
+    .Buttons({
+      fundingSource: fundingSource,
+      // Sets up the transaction when a payment button is clicked
+      createOrder: function () {
+        return fetch("/my-server/create-paypal-order", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // use the "body" param to optionally pass additional order information
+          // like product skus and quantities
+          body: JSON.stringify({
+            cart: [
+              {
+                sku: "YOUR_PRODUCT_STOCK_KEEPING_UNIT",
+                quantity: "YOUR_PRODUCT_QUANTITY",
+              },
+            ],
+          }),
+        })
+          .then((response) => response.json())
+          .then((order) => order.id);
+      },
+      // Finalize the transaction after payer approval
+      onApprove: function (data) {
+        return fetch("/my-server/capture-paypal-order", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            orderID: data.orderID,
+          }),
+        })
+          .then((response) => response.json())
+          .then((orderData) => {
+            // Successful capture! For dev/demo purposes:
+            console.log(
+              "Capture result",
+              orderData,
+              JSON.stringify(orderData, null, 2)
+            );
+            const transaction =
+              orderData.purchase_units[0].payments.captures[0];
+            alert(
+              "Transaction " +
+                transaction.status +
+                ": " +
+                transaction.id +
+                "\n\nSee console for all available details"
+            );
+            // When ready to go live, remove the alert and show a success message within this page. For example:
+            // var element = document.getElementById('paypal-button-container');
+            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+            // Or go to another URL:  actions.redirect('thank_you.html');
+          });
+      },
+    })
+    .render(`#${fundingSource}-button-container`);
+});
 
 // Listen for changes to the radio buttons
 document.querySelectorAll("input[name=payment-option]").forEach(function (el) {
   el.addEventListener("change", function (event) {
     // If PayPal is selected, show the PayPal button
     if (event.target.value === "paypal") {
-      document.body.querySelector("#alternate-button-container").style.display =
-        "none";
+      document.body.querySelector(
+        "#alternative-button-container"
+      ).style.display = "none";
       document.body.querySelector("#paypal-button-container").style.display =
         "block";
       document.body.querySelector("#card-button-container").style.display =
@@ -88,8 +83,9 @@ document.querySelectorAll("input[name=payment-option]").forEach(function (el) {
         "none";
     }
     if (event.target.value === "card") {
-      document.body.querySelector("#alternate-button-container").style.display =
-        "none";
+      document.body.querySelector(
+        "#alternative-button-container"
+      ).style.display = "none";
       document.body.querySelector("#card-button-container").style.display =
         "block";
       document.body.querySelector("#paypal-button-container").style.display =
@@ -98,8 +94,9 @@ document.querySelectorAll("input[name=payment-option]").forEach(function (el) {
         "none";
     }
     if (event.target.value === "paylater") {
-      document.body.querySelector("#alternate-button-container").style.display =
-        "none";
+      document.body.querySelector(
+        "#alternative-button-container"
+      ).style.display = "none";
       document.body.querySelector("#card-button-container").style.display =
         "none";
       document.body.querySelector("#paypal-button-container").style.display =
@@ -107,10 +104,11 @@ document.querySelectorAll("input[name=payment-option]").forEach(function (el) {
       document.body.querySelector("#paylater-button-container").style.display =
         "block";
     }
-    // If alternate funding is selected, show a different button
-    if (event.target.value === "alternate") {
-      document.body.querySelector("#alternate-button-container").style.display =
-        "block";
+    // If alternative funding is selected, show a different button
+    if (event.target.value === "alternative") {
+      document.body.querySelector(
+        "#alternative-button-container"
+      ).style.display = "block";
       document.body.querySelector("#paypal-button-container").style.display =
         "none";
       document.body.querySelector("#paylater-button-container").style.display =
@@ -122,7 +120,7 @@ document.querySelectorAll("input[name=payment-option]").forEach(function (el) {
 });
 
 // Hide non-PayPal button by default
-document.body.querySelector("#alternate-button-container").style.display =
+document.body.querySelector("#alternative-button-container").style.display =
   "none";
 document.body.querySelector("#card-button-container").style.display = "none";
 document.body.querySelector("#paylater-button-container").style.display =
